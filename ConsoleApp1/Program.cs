1248188e54bfc8c00fd434cf0151dad13c68332d
ArtonV <aveepalu@tktk.ee>
2023-07-25 19:13:35 +0300

    add week7 code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the operation (F/C):");
+
+char userOperator = Char.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Enter degree:");
+
+int firstNum = Int32.Parse(Console.ReadLine());
+
+
+
+static void ConverToCelsius(int a)
+
+{
+
+    Console.WriteLine($"{a} * 9 / 5 + 32");
+
+}
+
